439b3802eaaf3e49881a4c2f66e9fbf78f6282e5
yj-oh <holidayoh99@gmail.com>
2021-06-16 00:08:33 +0900

    [기능] 보드 | 이름 수정

--- src/Board.svelte
+++ src/Board.svelte
@@ -1,7 +1,73 @@
-<div>Board</div>
+<script>
+	import { boardList, activeBoard } from './store/stores';
+
+	let isEdit = false;
+
+	function toggleEdit() {
+		isEdit = !isEdit;
+	}
+
+	function handleInputBlur(e) {
+		if (e.key === 'Enter') {
+			e.target.blur();
+		}
+	}
+
+	function updateBoardName() {
+		boardList.update(list => {
+			list.map(board => {
+				if (board.id === $activeBoard.id) {
+					board.name = $activeBoard.name;
+				}
+			});
+			return list;
+		});
+		toggleEdit();
+	}
+</script>
+
+<section>
+	<header>
+		{#if isEdit}
+			<input
+				class='board-name'
+				required
+				autofocus
+				maxlength='50'
+				bind:value={$activeBoard.name}
+				on:blur={updateBoardName}
+				on:keydown={handleInputBlur}
+			/>
+		{:else}
+			<div class='board-name' on:click={toggleEdit}>
+				<span>{$activeBoard.name}</span>
+			</div>
+		{/if}
+	</header>
+</section>
 
 <style>
-	div {
+	section {
 		width: calc(100% - 200px);
 	}
+	.board-name {
+		padding: 2rem 0 1.5rem 1.5rem;
+		width: 100%;
+		height: 2rem;
+		font-size: 1.5rem;
+		font-weight: bold;
+	}
+    input.board-name {
+        border: none;
+    }
+	div.board-name {
+		display: inline-flex;
+		align-items: center;
+		cursor: pointer;
+	}
+    div.board-name span {
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+    }
 </style>
