d72890e3dacdd7aad7a634655aebc1fba38fc383
yj-oh <holidayoh99@gmail.com>
2021-06-17 09:39:19 +0900

    [추가] 보드 목록 | remove icon

--- src/components/BoardList.svelte
+++ src/components/BoardList.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Icon from 'svelte-awesome/components/Icon.svelte';
-	import { plus } from 'svelte-awesome/icons';
+	import { plus, trash } from 'svelte-awesome/icons';
 	import { boardList, activeBoard } from '../store/stores';
 	import { getInitBoard } from '../utils';
 
@@ -12,7 +12,7 @@
 	}
 
 	function changeActiveBoard(e) {
-		const id = e.target.dataset.id;
+		const id = e.currentTarget.dataset.id;
 		const newActiveBoard = $boardList.find(board => board.id === id);
 
 		activeBoard.set(newActiveBoard);
@@ -21,14 +21,15 @@
 
 <section>
 	<h3>📒 Online Post-it</h3>
-	<ul class='nav-board'>
+	<ul>
 		{#each $boardList as board (board.id)}
 			<li
 				class={board.id === $activeBoard.id ? 'active' : ''}
 				data-id={board.id}
 				on:click={changeActiveBoard}
 			>
-				<span>{board.name}</span>
+				<span class='board-name'>{board.name}</span>
+				<span class='delete-btn'><Icon data={trash} /></span>
 			</li>
 		{/each}
 		<li class='add-btn' on:click={addBoard}>
@@ -40,7 +41,7 @@
 <style>
     section {
         width: 200px;
-        background-color: rgb(226, 226, 226);
+        background-color:#e2e2e2;
         overflow-y: auto;
     }
     h3 {
@@ -48,29 +49,57 @@
         border-bottom: 1px solid rgb(210, 210, 210);
     }
     li {
-        display: flex;
-        align-items: center;
-        padding: 0.5rem 1.5rem;
+	    position: relative;
+        padding: 0.5rem 1rem 0.5rem 1.2rem;
 	    height: 2.5rem;
         line-height: 1.5rem;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
         cursor: pointer;
     }
     li:hover {
-        background-color: #c5c5c5;
+        background-color: #d0d0d0;
     }
     li.active {
-        background-color: #212121;
-        color: #ffffff;
+	    color: #e2e2e2ff;
+	    background-color: #212121;
     }
-    li.add-btn {
-	    justify-content: center;
-    }
-    li span {
+    .board-name {
         white-space: nowrap;
         text-overflow: ellipsis;
         overflow: hidden;
     }
-    :global(.nav-board svg) {
-	    flex-shrink: 0;
+    .delete-btn, .add-btn {
+	    color: #676767;
+    }
+    .delete-btn {
+        display: flex;
+        align-items: center;
+        position: absolute;
+        top: 10%;
+        right: 0.5rem;
+        padding: 3px 8px;
+        height: 80%;
+        border-radius: 3px;
+        opacity: 0;
+    }
+    li:hover .delete-btn {
+        opacity: 1;
+    }
+    li.active .delete-btn {
+	    background-color: #212121;
+    }
+    li.active .delete-btn:hover {
+        color: #c5c5c5;
+    }
+    .add-btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .delete-btn:hover,
+    .add-btn:hover {
+        color: #212121;
     }
 </style>
